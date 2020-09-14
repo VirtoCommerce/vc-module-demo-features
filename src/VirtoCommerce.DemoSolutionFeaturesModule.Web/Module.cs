68e964abba2084cabd5fc4f6410ce6565eb14f9b
Aleksandr Vishniakov <av@virtoway.com>
2020-09-14 11:53:12 +0200

    Revert "VP-4238: Seect default address as default on checkout (#1)" (#6)
    This reverts commit 5f6dcac5c17c85a61163cfc26e6f18b07be5eede.

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Module.cs
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Module.cs
@@ -3,14 +3,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using VirtoCommerce.CartModule.Data.Model;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.DemoSolutionFeaturesModule.Core;
-using VirtoCommerce.DemoSolutionFeaturesModule.Data.Models;
 using VirtoCommerce.DemoSolutionFeaturesModule.Data.Repositories;
-using VirtoCommerce.Platform.Core.Common;
 
 
 namespace VirtoCommerce.DemoSolutionFeaturesModule.Web
@@ -42,8 +39,6 @@ namespace VirtoCommerce.DemoSolutionFeaturesModule.Web
                     ModuleId = ModuleInfo.Id,
                     Name = x
                 }).ToArray());
-
-            AbstractTypeFactory<AddressEntity>.OverrideType<AddressEntity, CartAddressEntity>();
 
             // Ensure that any pending migrations are applied
             using (var serviceScope = appBuilder.ApplicationServices.CreateScope())
