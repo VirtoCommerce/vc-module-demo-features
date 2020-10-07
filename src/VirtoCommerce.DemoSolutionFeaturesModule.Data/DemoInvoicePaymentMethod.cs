764e792500d5473063fa89a3f93d36fa92043bd6
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2020-10-07 15:42:01 +0200

    Get logo url from Settings

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Data/DemoInvoicePaymentMethod.cs
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Data/DemoInvoicePaymentMethod.cs
@@ -1,17 +1,25 @@
 using System;
+using VirtoCommerce.DemoSolutionFeaturesModule.Core;
 using VirtoCommerce.PaymentModule.Core.Model;
 using VirtoCommerce.PaymentModule.Model.Requests;
+using VirtoCommerce.Platform.Core.Settings;
 
 namespace VirtoCommerce.DemoSolutionFeaturesModule.Data
 {
     public class DemoInvoicePaymentMethod : PaymentMethod
     {
-        public DemoInvoicePaymentMethod() : base("InvoicePaymentMethod")
+        public DemoInvoicePaymentMethod()
+            : base("InvoicePaymentMethod")
         {
         }
 
         [Obsolete("Need to use localized strings on clients side instead")]
-        public string Name => "Invoice";
+        public new string Name => "Invoice";
+
+        public new string LogoUrl => Settings?.GetSettingValue(
+            ModuleConstants.Settings.DemoInvoicePaymentMethod.Logo.Name,
+            ModuleConstants.Settings.DemoInvoicePaymentMethod.Logo.DefaultValue.ToString()
+        );
 
         public override PaymentMethodType PaymentMethodType => PaymentMethodType.Unknown;
 
