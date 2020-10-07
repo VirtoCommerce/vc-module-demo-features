0a8ca3ae7683a1ac9960c5f6f371eb6c9b4dc774
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2020-10-07 15:41:33 +0200

    Small refactoring

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Module.cs
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Module.cs
@@ -3,21 +3,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VirtoCommerce.CustomerModule.Core.Model;
+using VirtoCommerce.CustomerModule.Data.Model;
+using VirtoCommerce.CustomerModule.Data.Repositories;
+using VirtoCommerce.DemoSolutionFeaturesModule.Core;
+using VirtoCommerce.DemoSolutionFeaturesModule.Core.Models;
+using VirtoCommerce.DemoSolutionFeaturesModule.Data;
+using VirtoCommerce.DemoSolutionFeaturesModule.Data.Models;
+using VirtoCommerce.DemoSolutionFeaturesModule.Data.Repositories;
+using VirtoCommerce.DemoSolutionFeaturesModule.Data.Services;
+using VirtoCommerce.OrdersModule.Data.Services;
+using VirtoCommerce.PaymentModule.Core.Services;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Core.Settings;
-using VirtoCommerce.DemoSolutionFeaturesModule.Core;
-using VirtoCommerce.DemoSolutionFeaturesModule.Data.Repositories;
-using VirtoCommerce.CustomerModule.Core.Model;
-using VirtoCommerce.DemoSolutionFeaturesModule.Core.Models;
-using VirtoCommerce.Platform.Core.Common;
-using VirtoCommerce.CustomerModule.Data.Repositories;
-using VirtoCommerce.CustomerModule.Data.Model;
-using VirtoCommerce.DemoSolutionFeaturesModule.Data.Models;
-using VirtoCommerce.OrdersModule.Data.Services;
-using VirtoCommerce.DemoSolutionFeaturesModule.Data.Services;
-using VirtoCommerce.PaymentModule.Core.Services;
-using VirtoCommerce.DemoSolutionFeaturesModule.Data;
 
 namespace VirtoCommerce.DemoSolutionFeaturesModule.Web
 {
@@ -49,26 +49,24 @@ namespace VirtoCommerce.DemoSolutionFeaturesModule.Web
             // register invoicePaymentMethod
             var paymentMethodsRegistrar = appBuilder.ApplicationServices.GetRequiredService<IPaymentMethodsRegistrar>();
             paymentMethodsRegistrar.RegisterPaymentMethod<DemoInvoicePaymentMethod>();
-            settingsRegistrar.RegisterSettingsForType(ModuleConstants.Settings.DemoInvoicePaymentMethod.AllSettings, typeof(DemoInvoicePaymentMethod).Name);
+            settingsRegistrar.RegisterSettingsForType(ModuleConstants.Settings.DemoInvoicePaymentMethod.AllSettings, nameof(DemoInvoicePaymentMethod));
 
             // register permissions
             var permissionsProvider = appBuilder.ApplicationServices.GetRequiredService<IPermissionsRegistrar>();
             permissionsProvider.RegisterPermissions(ModuleConstants.Security.Permissions.AllPermissions.Select(x =>
-                new Permission()
+                new Permission
                 {
                     GroupName = "VirtoCommerceDemoSolutionFeaturesModule",
                     ModuleId = ModuleInfo.Id,
-                    Name = x
+                    Name = x,
                 }).ToArray());
 
             // Ensure that any pending migrations are applied
             using (var serviceScope = appBuilder.ApplicationServices.CreateScope())
+            using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<CustomerDemoDbContext>())
             {
-                using (var dbContext = serviceScope.ServiceProvider.GetRequiredService<CustomerDemoDbContext>())
-                {
-                    dbContext.Database.EnsureCreated();
-                    dbContext.Database.Migrate();
-                }
+                dbContext.Database.EnsureCreated();
+                dbContext.Database.Migrate();
             }
         }
 
