68bc3935ec517b47c34148de049ade19af810e98
trueboroda <superiorboroda@gmail.com>
2020-10-22 10:01:10 +0300

    VP-5611: Override GetShoppingCartsByIdsAsync of Cart repository to load configured groups data

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Data/Repositories/DemoCartRepository.cs
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Data/Repositories/DemoCartRepository.cs
@@ -1,12 +1,35 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using VirtoCommerce.CartModule.Data.Model;
 using VirtoCommerce.CartModule.Data.Repositories;
+using VirtoCommerce.DemoSolutionFeaturesModule.Data.Models;
 
 namespace VirtoCommerce.DemoSolutionFeaturesModule.Data.Repositories
 {
     public class DemoCartRepository : CartRepository
     {
+
+        public IQueryable<DemoCartConfiguredGroupEntity> ConfiguredGroups => DbContext.Set<DemoCartConfiguredGroupEntity>();
+
+        public IQueryable<DemoCartLineItemConfiguredGroupEntity> LineItmemConfiguredGroups => DbContext.Set<DemoCartLineItemConfiguredGroupEntity>();
+
         public DemoCartRepository(DemoCartDbContext dbContext)
             : base(dbContext)
         {
+        }
+
+        public override Task<ShoppingCartEntity[]> GetShoppingCartsByIdsAsync(string[] ids, string responseGroup = null)
+        {
+            var result = base.GetShoppingCartsByIdsAsync(ids, responseGroup);
+
+            ConfiguredGroups.Where(x => ids.Contains(x.ShoppingCartId)).Load();
+
+            var groupIds = ConfiguredGroups.Select(x => x.Id).ToArray();
+
+            LineItmemConfiguredGroups.Where(x => groupIds.Contains(x.GroupId)).Load();
+
+            return result;
         }
     }
 }
