d06a103b0c7cee82fb8e004eb3ab934e370ac566
trueboroda <superiorboroda@gmail.com>
2020-12-02 17:23:24 +0300

    Remove customer segments caching

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Data/Caching/DemoCustomerSegmentSearchCacheRegion.cs
+++
@@ -1,9 +0,0 @@
-using VirtoCommerce.Platform.Core.Caching;
-
-namespace VirtoCommerce.DemoSolutionFeaturesModule.Data.Caching
-{
-    public class DemoCustomerSegmentSearchCacheRegion: CancellableCacheRegion<DemoCustomerSegmentSearchCacheRegion>
-    {
-        
-    }
-}
