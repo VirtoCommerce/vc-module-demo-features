dbedc10f960cd8c652655dc455d0a28d1f8bdc4a
trueboroda <superiorboroda@gmail.com>
2020-12-16 16:57:00 +0300

    Use feature service to toogle ConfigurableProduct feature.

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/module.js
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/module.js
@@ -27,12 +27,14 @@ angular.module(moduleName, [])
             }
         }
     }])
-    .run(['virtoCommerce.catalogModule.itemTypesResolverService',
-        function ( itemTypesResolverService) { 
-        itemTypesResolverService.registerType({
-            itemType: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.title',
-            description: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.description',
-            productType: 'Configurable',
-            icon: 'fa-cogs'
-        });
+    .run(['virtoCommerce.catalogModule.itemTypesResolverService', 'virtoCommerce.demoFeatures.featureManager',
+        function (itemTypesResolverService, featureManager) {
+            featureManager.isFeatureEnabled('ConfigurableProduct').then(() => {
+                itemTypesResolverService.registerType({
+                    itemType: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.title',
+                    description: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.description',
+                    productType: 'Configurable',
+                    icon: 'fa-cogs'
+                });
+            });
     }]);
