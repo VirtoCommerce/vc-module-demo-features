d69b97e51f44556b5c25bbd816e831dd07997788
trueboroda <superiorboroda@gmail.com>
2020-12-16 17:04:59 +0300

    Extract feature manager service to separated file

---
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/services/feature-manager.js
@@ -0,0 +1,24 @@
+var moduleName = "virtoCommerce.DemoSolutionFeaturesModule";
+
+angular.module(moduleName)
+    .factory('virtoCommerce.demoFeatures.featureManager', ['$q', '$http', function ($q, $http) {
+        return {
+            isFeatureEnabled: (featureName) => {
+                var deferred = $q.defer();
+
+                $http({ method: 'GET', url: `api/demo/features/${featureName}` })
+                    .then(
+                        (response) => {
+                            var result = response.data;
+                            if (result) {
+                                deferred.resolve();
+                            } else {
+                                deferred.reject();
+                            }
+                        }
+                    );
+
+                return deferred.promise;
+            }
+        }
+    }]);
