30daa3b28422f19419be25c03d6673f645f624c9
Anton <superiorboroda@gmail.com>
2020-12-21 15:01:27 +0300

    VDS-410, VDS-411 Create configurable product (#39)
    * Register 'Configurable' type of product
    
    * Use feature service to toogle ConfigurableProduct feature.
    
    * Extract feature manager service to separated file
    
    * Remove comment

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/module.js
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/module.js
@@ -6,24 +6,14 @@ if (AppDependencies !== undefined) {
 }
 
 angular.module(moduleName, [])
-    .factory('virtoCommerce.demoFeatures.featureManager', ['$q', '$http', function($q, $http) {
-        return {
-            isFeatureEnabled: (featureName) => {
-                var deferred = $q.defer();
-
-                $http({ method: 'GET', url: `api/demo/features/${featureName}` })
-                    .then(
-                        (response) => {
-                            var result = response.data;
-                            if (result) {
-                                deferred.resolve();
-                            } else {
-                                deferred.reject();
-                            }
-                        }
-                );
-
-                return deferred.promise;
-            }
-        }
+    .run(['virtoCommerce.catalogModule.itemTypesResolverService', 'virtoCommerce.demoFeatures.featureManager',
+        function (itemTypesResolverService, featureManager) {
+            featureManager.isFeatureEnabled('ConfigurableProduct').then(() => {
+                itemTypesResolverService.registerType({
+                    itemType: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.title',
+                    description: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.description',
+                    productType: 'Configurable',
+                    icon: 'fa-cogs'
+                });
+            });
     }]);
