aad65b2e6b9a162fb7426115501c15e47ff23110
trueboroda <superiorboroda@gmail.com>
2020-12-23 12:27:50 +0300

    Add migration

---
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Data/Migrations/20201223091129_ExtendProductPartItemWithPriority.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.DemoSolutionFeaturesModule.Data.Migrations
+{
+    public partial class ExtendProductPartItemWithPriority : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "DemoProductPartItem",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "DemoProductPartItem");
+        }
+    }
+}
