79b2961d56fe17253bf4bfe23a6e28c2509487d1
Ilya Litvinenko <ilya.ivanov6@gmail.com>
2020-12-24 14:37:30 +0200

    VDS-502: Add a part to a configurable product  (#47)
    * VDS-438: Add widget and blade
    
    * Fix Sonar issues
    
    Format html
    
    * VDS-412: Add drag & drop
    
    * Fix issues after upgrade
    
    * Remove mock data
    
    * Fix code review issues
    
    * Fix end new line
    
    * Fix editorconfig
    
    * Fix module name and description
    
    * VDS-438: Fix bugs
    
    * Fix symbol
    
    * VDS-502: Add a part to a configurable product
    
    * VDS-502: Fix confirmation dialog message
    
    * Change 'undefined' to false in 'SaveChanges' function
    
    * Rename template and controller, show only one toolbar command in blade
    
    * Replace update by save
    
    Co-authored-by: Aleksandr Vishniakov <av@virtoway.com>
    Co-authored-by: Dmitry Pushnitsa <d.pushnitsa@gmail.com>

---
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/blades/product-part-detail.js
@@ -0,0 +1,79 @@
+angular.module('virtoCommerce.DemoSolutionFeaturesModule')
+.controller('virtoCommerce.DemoSolutionFeaturesModule.productPartDetailController',
+    ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.DemoSolutionFeaturesModule.productPartsApi',
+    function ($scope, bladeNavigationService, productPartsApi) {
+        const blade = $scope.blade;
+        blade.headIcon = 'fa-cogs';
+        blade.toolbarCommands = [];
+
+        if (blade.isNew) {
+            blade.toolbarCommands.push({
+                name: "platform.commands.create", icon: 'fa fa-check',
+                executeMethod: () => $scope.saveChanges(),
+                canExecuteMethod: () => blade.isNew && $scope.canSave(),
+                permission: 'catalog:create'
+            });
+        } else {
+            blade.toolbarCommands.push({
+                name: "platform.commands.save", icon: 'fa fa-save',
+                executeMethod: () => $scope.saveChanges(),
+                canExecuteMethod: () => !blade.isNew && $scope.canSave(),
+                permission: 'catalog:update'
+            });
+        }
+
+        blade.refresh = (parentRefresh) => {
+            if (blade.isNew) {
+                blade.originalEntity = {
+                    configuredProductId: blade.configuredProductId,
+                    priority: blade.partsLength + 1,
+                    maxQuantity: 0,
+                    minQuantity: 0
+                };
+                blade.currentEntity = angular.copy(blade.originalEntity);
+                blade.isLoading = false;
+            } else {
+                blade.currentEntity = angular.copy(blade.originalEntity);
+                blade.isLoading = false;
+            }
+
+            if (parentRefresh) {
+                blade.parentBlade.refresh();
+            }
+        };
+
+        blade.onClose = (closeCallback) => {
+            bladeNavigationService.showConfirmationIfNeeded(isDirty() && !blade.isNew && !isPriorityChanged(), $scope.isValid(), blade, $scope.saveChanges, closeCallback, "demoSolutionFeaturesModule.dialogs.product-part-create.title", "demoSolutionFeaturesModule.dialogs.product-part-create.message");
+        };
+
+        var formScope;
+        $scope.setForm = (form) => { formScope = form; };
+
+        $scope.isValid = () => formScope && formScope.$valid;
+
+        $scope.canSave = () => isDirty() && $scope.isValid();
+
+        function isDirty() {
+            return !angular.equals(blade.currentEntity, blade.originalEntity);
+        }
+
+        function isPriorityChanged() {
+            return !angular.equals(blade.currentEntity.priority, blade.originalEntity.priority);
+        }
+
+        $scope.saveChanges = () => {
+            productPartsApi.save({}, [blade.currentEntity], () => {
+                if (blade.isNew) {
+                    blade.isNew = false;
+                }
+                blade.refresh(true);
+                $scope.closeBlade();
+            });
+        }
+
+        $scope.closeBlade = () => {
+            bladeNavigationService.closeBlade(blade);
+        };
+
+        blade.refresh();
+    }]);
