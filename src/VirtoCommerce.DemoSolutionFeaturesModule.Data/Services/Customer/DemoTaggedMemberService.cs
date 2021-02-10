142c79a13b9bf19e8fa778fc7e7b8bfdf9e0c427
Anton Veretennikov <superiorboroda@gmail.com>
2021-02-10 19:19:01 +0300

    Remove using

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Data/Services/Customer/DemoTaggedMemberService.cs
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Data/Services/Customer/DemoTaggedMemberService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
 using VirtoCommerce.CustomerModule.Data.Caching;
-using VirtoCommerce.DemoSolutionFeaturesModule.Core.Events.Catalog;
 using VirtoCommerce.DemoSolutionFeaturesModule.Core.Events.Customer;
 using VirtoCommerce.DemoSolutionFeaturesModule.Core.Models.Customer;
 using VirtoCommerce.DemoSolutionFeaturesModule.Core.Services.Customer;
