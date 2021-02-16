692d47526445ecb4e4f4a4239324871d58afcbce
Dmitry Pushnitsa <2745590@gmail.com>
2021-02-16 13:47:02 +0200

    VDS-815: Sample subscription to event (#64)
    * Sample subscription to event
    
    * Improve sample
    
    * VDS-815: Create featureManagerSubscriber service
    
    * Rename variable
    
    * Rename file
    
    * Rename callbacks variable
    
    * Move isAuthenticated condition to featureManagerSubscriber
    
    * Rename result.subscribeToLoginAction
    
    * Add a description to 'featureManagerSubscriber' init function
    
    * Add changes to function description
    
    Co-authored-by: Ilia <ilya.ivanov6@gmail.com>

--- src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/module.js
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/module.js
@@ -6,21 +6,10 @@ if (AppDependencies !== undefined) {
 }
 
 angular.module(moduleName, [])
-    .run(['$rootScope', '$window', 'virtoCommerce.catalogModule.itemTypesResolverService', 'platformWebApp.widgetService', 'virtoCommerce.demoFeatures.featureManager',
-        function ($rootScope, $window, itemTypesResolverService, widgetService, featureManager) {
-            // Refresh page & reinitialize application after login/logout.
-            // We need this workaround to toggle features depending on current user permissions
-            // It doesn't work by default because blades, widgets registration occurs during app initialization
-            let isFreshPageLoad = true;
-            $rootScope.$on('loginStatusChanged', function(_, authContext) {
-                if (!isFreshPageLoad && authContext.isAuthenticated) {
-                    $window.location.reload();
-                }
-                isFreshPageLoad = false;
-            });
-
-            const configurableProductType = 'Configurable';
-            featureManager.isFeatureEnabled('ConfigurableProduct').then(() => {
+    .run(['virtoCommerce.catalogModule.itemTypesResolverService', 'platformWebApp.widgetService', 'virtoCommerce.demoFeatures.featureManagerSubscriber',
+        function (itemTypesResolverService, widgetService, featureManagerSubscriber) {
+            featureManagerSubscriber.onLoginStatusChanged('ConfigurableProduct', () => {
+                const configurableProductType = 'Configurable';
                 itemTypesResolverService.registerType({
                     itemType: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.title',
                     description: 'demoSolutionFeaturesModule.blades.categories-items-add.menu.configurable-product.description',
---
+++ src/VirtoCommerce.DemoSolutionFeaturesModule.Web/Scripts/services/featureManagerSubscriber.js
@@ -0,0 +1,40 @@
+var moduleName = "virtoCommerce.DemoSolutionFeaturesModule";
+
+angular.module(moduleName)
+    .factory('virtoCommerce.demoFeatures.featureManagerSubscriber', ['$rootScope', 'virtoCommerce.demoFeatures.featureManager', '$window', function ($rootScope, featureManager, $window) {
+        var result = {};
+
+        result.callbacksGroupedByFeatureName = [];
+        result.onLoginStatusChanged = (featureName, callback) => {
+            if (!result.callbacksGroupedByFeatureName[featureName]) {
+                result.callbacksGroupedByFeatureName[featureName] = [];
+            }
+            if (typeof callback === 'function') {
+                result.callbacksGroupedByFeatureName[featureName].push(callback);
+            }
+        };
+
+        function initialize() {
+            //This function runs only on 'login'/'sign out' event, but the featureManager calls 'isFeatureEnabled' only at the time of login
+            //and then features are registered only if they are available to the user,
+            //when we sign out - we are just reloading
+            $rootScope.$on('loginStatusChanged',
+                (_, authContext) => {
+                    if (!authContext.isAuthenticated) {
+                        $window.location.reload();
+                    }
+
+                    for (const [featureName, callbacks] of Object.entries(result.callbacksGroupedByFeatureName)) {
+                        featureManager.isFeatureEnabled(featureName).then(() => {
+                            angular.forEach(callbacks, callback => {
+                                callback();
+                            })
+                        })
+                      }
+                });
+        }
+
+        initialize();
+
+        return result;
+    }]);
